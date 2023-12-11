f60c220340849b28501c3793c1891066e5499af9
menulln <39300523+menulln@users.noreply.github.com>
2023-12-11 19:22:47 +0100

    Add gameStart functionality

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const wordElement = document.getElementsByClassName('word')[0];
 const inputElement = document.getElementsByClassName('input')[0];
 
 let score = 0;
+let gameStarted = false;
+
+function startGame() {
+    inputElement.removeEventListener('input', startGame);
+    gameStarted = true
+}
+
+inputElement.addEventListener('input', startGame);
