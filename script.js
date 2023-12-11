1da8c8b57228968db161ba00f96a0a96c10723aa
menulln <39300523+menulln@users.noreply.github.com>
2023-12-11 19:14:07 +0100

    Define element variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const scoreElement = document.getElementsByClassName('score')[0];
+const timerElement = document.getElementsByClassName('timer')[0];
+const wordElement = document.getElementsByClassName('word')[0];
+const inputElement = document.getElementsByClassName('input')[0];
+
+let score = 0;
